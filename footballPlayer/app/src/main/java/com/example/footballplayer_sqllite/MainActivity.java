447a93d0cb15987c4cf69280e4501a7d1555aac5
Aderocky <113891125+Aderocky@users.noreply.github.com>
2023-03-13 09:32:47 +0700

    push file

--- footballPlayer/app/src/main/java/com/example/footballplayer_sqllite/MainActivity.java
+++ footballPlayer/app/src/main/java/com/example/footballplayer_sqllite/MainActivity.java
@@ -1,22 +1,34 @@
 package com.example.footballplayer_sqllite;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    myDataClassHelper mydb;
+    private myDataClassHelper mydb;
     private FloatingActionButton fab_tambah;
+    private RecyclerView rvPlayer;
+    private AdapterFootbalPlayer adPlayer ;
+    private ArrayList<String> arrNama, arrNomor , arrKlub;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        rvPlayer = findViewById(R.id.rv_player);
         mydb = new myDataClassHelper(MainActivity.this);
         fab_tambah = findViewById(R.id.fab_tambah);
 
@@ -28,4 +40,38 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        tampilPlayer();
+    }
+
+    private void SqlLiteToArrayList (){
+        Cursor varCursor = mydb.bacadataPlayer();
+        if(varCursor.getCount() == 0){
+            Toast.makeText(this, "Tidak Ada Data", Toast.LENGTH_SHORT).show();
+        }else{
+            while (varCursor.moveToNext()){
+                arrNama.add(varCursor.getString(1));
+                arrNomor.add(varCursor.getString(2));
+                arrKlub.add(varCursor.getString(3));
+            }
+        }
+    }
+    private void tampilPlayer (){
+        arrNama = new ArrayList<>();
+        arrNomor = new ArrayList<>();
+        arrKlub = new ArrayList<>();
+
+        SqlLiteToArrayList();
+
+        adPlayer = new AdapterFootbalPlayer(MainActivity.this,arrNama,arrNomor,arrKlub);
+
+        rvPlayer.setLayoutManager(new LinearLayoutManager(
+                MainActivity.this
+        ));
+        rvPlayer.setAdapter(adPlayer);
+    }
+
 }
